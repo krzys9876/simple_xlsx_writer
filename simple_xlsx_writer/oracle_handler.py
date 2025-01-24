c3c0125280be033eecbff282e0f0023a961b2e81
Krzysztof Ruta <krzysztof.ruta@play.pl>
2025-01-24 10:26:33 +0100

    add postgresql, reorganize common functions

--- simple_xlsx_writer/oracle_handler.py
+++ simple_xlsx_writer/oracle_handler.py
@@ -1,9 +1,8 @@
-from types import NoneType
 import sys
 
 import oracledb
 import datetime
-from simple_xlsx_writer import writer
+from simple_xlsx_writer import common
 
 
 # See:
@@ -22,57 +21,15 @@ def get_sysdate(user: str, password: str, dsn: str) -> datetime.datetime:
             return res[0]
 
 
-def get_data_from_query(query: str, user: str, password: str, dsn: str, custom_params = None) -> []:
+def get_data_from_query(query: str, user: str, password: str, dsn: str, debug: bool = False, custom_params = None) -> []:
     __init_oracle_version__()
 
-    params = writer.update_params(custom_params)
-
-    data = []
-    date_format = params["python_date_format"]
-    datetime_format = params["python_datetime_format"]
-    datetime_remove_zeros = params["python_datetime_remove_zeros"]
-    datetime_remove_zeros_pattern = params["python_datetime_remove_zeros_pattern"]
-    headers = params["headers"]
     with oracledb.connect(user=user, password=password, dsn=dsn) as connection:
-        with connection.cursor() as cursor:
-            result = cursor.execute(query)
-
-            if headers:
-                row = []
-                for c in result.description:
-                    row.append(c[0])
-                data.append(row)
-
-            for r in result:
-                row=[]
-                for cell in r:
-                    if type(cell)==int or type(cell)==float:
-                        row.append(cell)
-                    elif type(cell)==str:
-                        row.append(writer.escape_invalid_chars(cell))
-                    elif type(cell)==datetime.datetime:
-                        txt = cell.strftime(datetime_format)
-                        if datetime_remove_zeros:
-                            txt = txt.replace(datetime_remove_zeros_pattern, "")
-                            row.append(txt)
-                    elif type(cell) == datetime.date:
-                        row.append(cell.strftime(date_format))
-                    elif type(cell)==NoneType:
-                        row.append("")
-                    else:
-                        raise TypeError(f"Unsupported data type found in cell {cell} of type {type(cell)}")
-                data.append(row)
+        data = common.get_data_from_query(connection, query, debug, custom_params)
     return data
 
 
 def write_query(query: str, base_path: str, target_file_name: str, user: str, password: str, dsn: str,
                 debug: bool = False, custom_params = None) -> None:
-    if debug:
-        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+ ": executing query")
-    data = get_data_from_query(query,user,password,dsn, custom_params)
-    if debug:
-        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+ ": writing file")
-    writer.write_raw_data(base_path, target_file_name, data, debug, custom_params)
-    if debug:
-        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+ ": finished")
-
+    data = get_data_from_query(query,user,password,dsn, debug, custom_params)
+    common.write_data(base_path, target_file_name, data, debug, custom_params)
