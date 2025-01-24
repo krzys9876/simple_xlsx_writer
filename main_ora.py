dd2058c2fe607ee529913fae3a1b0b1a4446ac77
Krzysztof Ruta <krzysztof.ruta@play.pl>
2025-01-24 10:47:27 +0100

    comments

--- main_ora.py
+++ main_ora.py
@@ -7,16 +7,18 @@ from simple_xlsx_writer import oracle_handler
 def main():
     username = input("username: ")
     password = getpass.getpass()
-    dh_url = input("DSN: ")
+    dsn = input("DSN: ")
 
-    print("db time: "+oracle_handler.get_sysdate(username,password,dh_url).strftime("%Y-%m-%d %H:%M:%S"))
+    # verify connection
+    print("db time: "+oracle_handler.get_sysdate(username,password,dsn).strftime("%Y-%m-%d %H:%M:%S"))
 
     base_path = os.path.dirname(__file__)
 
     writer.write_dummy(base_path, "dummy01")
 
+    # fetch all tables' metadata
     query = "select * from all_tables"
-    oracle_handler.write_query(query, base_path, "all_tables_ora", username, password, dh_url)
+    oracle_handler.write_query(query, base_path, "all_tables_ora", username, password, dsn)
 
 
 if __name__ == '__main__':
--- main_pg.py
+++ main_pg.py
@@ -11,12 +11,14 @@ def main():
     port = int(input("port: "))
     dbname = input("database name: ")
 
+    # verify connection
     print("db time: "+postgresql_handler.get_sysdate(username,password,host,port,dbname).strftime("%Y-%m-%d %H:%M:%S"))
 
     base_path = os.path.dirname(__file__)
 
     writer.write_dummy(base_path, "dummy02")
 
+    # fetch all tables' metadata
     query = "select * from information_schema.tables"
     postgresql_handler.write_query(query, base_path, "all_tables_pg", username, password, host, port, dbname)
 
